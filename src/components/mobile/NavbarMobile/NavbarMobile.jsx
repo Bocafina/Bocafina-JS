390c9bc7602f801fc06522e8f80f1d4a52297ee2
Pierre-Marie <pierremariemarchio@gmail.com>
2024-05-24 15:16:44 +0200

    [chore] installed bootstrap scss

--- src/components/mobile/NavbarMobile/NavbarMobile.jsx
+++ src/components/mobile/NavbarMobile/NavbarMobile.jsx
@@ -1,13 +1,26 @@
-import {  }
+import './navbarMobile.scss'
+import { Link } from "react-router-dom";
+import  Nav  from "react-bootstrap/Nav";
+import { NavItem } from "react-bootstrap";
 
 const NavbarMobile = () => {
-
-    return (
-        <>
-         
-        </>
-    )
-
+  return (
+    <>
+   
+      <Nav className="justify-content-center">
+        <NavItem>
+          <Link to="/">home</Link>  
+        </NavItem>
+        <NavItem>
+          <Link to="/search">Tu veux quoi ?</Link>  
+        </NavItem>
+        
+        <Link to="/cookbook">Le livre de recettes de mamie</Link>
+        <Link to="/menus">On va planifier, quoi</Link>
+        <Link to="/groceries">Les courses tavu</Link>
+      </Nav>
+    </>
+  );
 };
 
-export default NavbarMobile 
+export default NavbarMobile;
