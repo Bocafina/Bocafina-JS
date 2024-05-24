3f2eed2ace4470131ed1c8633f1a78b1dd147468
Sam Sabat <samnsabat@gmail.com>
2024-05-24 17:49:49 +0200

    implement scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from 'react-router-dom'
 
-import HomeScreen from '../screen/HomeScreen'
-import SearchScreen from '../screen/SearchScreen';
-import CookBookScreen from '../screen/CookBookScreen';
-import MenusScreen from '../screen/MenusScreen';
-import GroceriesScreen from '../screen/GroceriesScreen';
-import PageNotFound from '../screen/PageNotFound';
+import HomeScreen from './screen/HomeScreen'
+import SearchScreen from './screen/SearchScreen';
+import CookBookScreen from './screen/CookBookScreen';
+import MenusScreen from './screen/MenusScreen';
+import GroceriesScreen from './screen/GroceriesScreen';
+import PageNotFound from './screen/PageNotFound';
 
 import '../assets/styles/App.scss';
 
