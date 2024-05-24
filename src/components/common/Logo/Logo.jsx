e2de78c15e5702e84c6d7d5b557e8ab0002a0a6f
Sam Sabat <samnsabat@gmail.com>
2024-05-24 14:45:50 +0200

    work in process logo

---
+++ src/components/common/Logo/Logo.jsx
@@ -0,0 +1,11 @@
+import "../Logo/logo.css";
+
+const Logo = () => {
+  return (
+    <>
+    <h1>YO</h1>
+    </>
+  )
+};
+
+export default Logo
--- src/screen/HomeScreen.jsx
+++ src/screen/HomeScreen.jsx
@@ -1,6 +1,9 @@
+import Logo from "../components/common/Logo/Logo";
+
 export default function HomeScreen() {
   return (
     <>
+      <Logo />
       <h1>Home Page</h1>
     </>
   );
