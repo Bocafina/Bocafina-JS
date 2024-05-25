505d80796de263ce4064b18ba96d2321acb1a1e9
Sam Sabat <samnsabat@gmail.com>
2024-05-25 18:03:44 +0200

    test scss in progress

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import MenusScreen from './screen/MenusScreen';
 import GroceriesScreen from './screen/GroceriesScreen';
 import PageNotFound from './screen/PageNotFound';
 
-import '../assets/styles/App.scss';
+import './assets/styles/App.scss';
 
 
 function App() {
