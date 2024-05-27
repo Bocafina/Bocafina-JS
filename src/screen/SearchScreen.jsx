63012e0caf04b79b74a6854075470e0a9bbec08a
Pierre-Marie <pierremariemarchio@gmail.com>
2024-05-27 16:48:55 +0200

    [component] started RecipesFiltersBar

--- src/screen/SearchScreen.jsx
+++ src/screen/SearchScreen.jsx
@@ -1,35 +1,41 @@
 import RecipesCardDisplay from "../components/common/RecipesCardDisplay/RecipesCardDisplay";
+import RecipesFiltersBar from "../components/common/RecipesFiltersBar/RecipesFiltersBar";
 
 export default function SearchScreen() {
   return (
     <>
-      <RecipesCardDisplay
-        hasHeader={false}
-        imageSrc="./src/assets/img/imgTest.png"
-        profilName="Nom de profil"
-        recipeTitle="Titre de la recette"
-      />
+      <div className="container-fluid">
 
-      <RecipesCardDisplay
-        hasHeader={false}
-        imageSrc="./src/assets/img/imgTest.png"
-        profilName="Nom de profil"
-        recipeTitle="Titre de la recette"
-      />
+        <RecipesFiltersBar/>
 
-      <RecipesCardDisplay
-        hasHeader={false}
-        imageSrc="./src/assets/img/imgTest.png"
-        profilName="Nom de profil"
-        recipeTitle="Titre de la recette"
-      />
+        <RecipesCardDisplay
+          hasHeader={false}
+          imageSrc="./src/assets/img/imgTest.png"
+          profilName="Nom de profil"
+          recipeTitle="Titre de la recette"
+        />
 
-      <RecipesCardDisplay
-        hasHeader={false}
-        imageSrc="./src/assets/img/imgTest.png"
-        profilName="Nom de profil"
-        recipeTitle="Titre de la recette"
-      />
+        <RecipesCardDisplay
+          hasHeader={false}
+          imageSrc="./src/assets/img/imgTest.png"
+          profilName="Nom de profil"
+          recipeTitle="Titre de la recette"
+        />
+
+        <RecipesCardDisplay
+          hasHeader={false}
+          imageSrc="./src/assets/img/imgTest.png"
+          profilName="Nom de profil"
+          recipeTitle="Titre de la recette"
+        />
+
+        <RecipesCardDisplay
+          hasHeader={false}
+          imageSrc="./src/assets/img/imgTest.png"
+          profilName="Nom de profil"
+          recipeTitle="Titre de la recette"
+        />
+      </div>
     </>
   );
 }
