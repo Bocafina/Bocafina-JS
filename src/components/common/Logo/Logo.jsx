81121b9df538f3b10f8588ceb835fa722f290962
Sam Sabat <samnsabat@gmail.com>
2024-05-27 18:21:21 +0200

    bug fixed

--- src/components/common/Logo/Logo.jsx
+++ src/components/common/Logo/Logo.jsx
@@ -5,24 +5,26 @@ import "./logo.scss";
 const Logo = () => {
 	return (
 		<>
-				<div className="wrapper mt-3 rounded-3 position-relative">
-					<Row className="sizeWelcome me-2 d-flex justify-content-end">Bienvenue sur</Row>
-					<Row className="flex-nowrap">
-						<Col className="ms-1 pb-2 sizeLogo">
-							<Image
-								className=""
-								src="../../src/assets/img/logo.png"
-								alt="Bocafina logo"
-							/>
-						</Col>
-						<Col className="sizeHelloCat">
-							<Image
-								src="../../src/assets/img/hello.png"
-								alt="Simon's cat hello"
-							/>
-						</Col>
-					</Row>
-				</div>
+			<div className="wrapper mt-3 rounded-3 position-relative">
+				<Row className="sizeWelcome me-2 d-flex justify-content-end">
+					Bienvenue sur
+				</Row>
+				<Row className="flex-nowrap">
+					<Col className="ms-1 pb-2 sizeLogo">
+						<Image
+							className=""
+							src="../../src/assets/img/logo.png"
+							alt="Bocafina logo"
+						/>
+					</Col>
+					<Col className="sizeHelloCat">
+						<Image
+							src="../../src/assets/img/hello.png"
+							alt="Simon's cat hello"
+						/>
+					</Col>
+				</Row>
+			</div>
 		</>
 	);
 };
