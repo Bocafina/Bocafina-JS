8bf92bcd726607fc168b1ed97a7826d0fb14cd3b
Pierre-Marie <pierremariemarchio@gmail.com>
2024-05-28 14:52:45 +0200

    [hotfix] warped listbtn in button

--- src/components/mobile/ListWrapper/ListBtn/ListBtn.jsx
+++ src/components/mobile/ListWrapper/ListBtn/ListBtn.jsx
@@ -5,20 +5,18 @@ import ListGroup from "react-bootstrap/ListGroup";
 import "./listBtn.scss";
 
 const ListBtn = (props) => {
-	return (
-		<>
-			<ListGroup.Item className="text-secondary listBtnStyle">
-				<div className="flex-between">
-					<span>{props.dayName}</span>
-					<Image
-						className=""
-						src="../../src/assets/svg/rightarrowicon.svg"
-						alt="Icone flèche vers la droite"
-					/>
-				</div>
-			</ListGroup.Item>
-		</>
-	);
+  return (
+    <>
+      <button>
+        <ListGroup.Item className="text-secondary listBtnStyle">
+          <div className="flex-between">
+            <span>{props.dayName}</span>
+            <Image className="" src="../../src/assets/svg/rightarrowicon.svg" alt="Icone flèche vers la droite" />
+          </div>
+        </ListGroup.Item>
+      </button>
+    </>
+  );
 };
 
 export default ListBtn;
