4527f811950e7077477e9cff2833ac957cf76104
Pierre-Marie <pierremariemarchio@gmail.com>
2024-05-29 11:54:44 +0200

    [chore] working

--- src/components/common/BtnBlackGreen/BtnBlackGreen.jsx
+++ src/components/common/BtnBlackGreen/BtnBlackGreen.jsx
@@ -4,9 +4,12 @@ import "./btnBlackGreen.scss";
 const BtnBlackGreen = (props) => {
   return (
     <>
-      <Button className="text-secondary rounded-5 border-1 border btn-BG" onClick={() => props.func()}>
+    <div className="btn-BG">
+      <Button className="text-secondary rounded-5 border-1 border" onClick={() => props.func()}>
         {props.btnName}
       </Button>
+    </div>
+      
     </>
   );
 };
--- src/components/common/RecipesFiltersBar/RecipesFiltersBtn/RecipesFiltersBtn.jsx
+++ src/components/common/RecipesFiltersBar/RecipesFiltersBtn/RecipesFiltersBtn.jsx
@@ -2,16 +2,15 @@ import { Button } from "react-bootstrap";
 import "./recipesFiltersBtn.scss";
 
 const RecipesFiltersBtn = (props) => {
-	return (
-		<>
-			<Button
-				className="text-secondary rounded-5 border-1 border border-secondary"
-				onClick={() => props.func()}
-			>
-				{props.btnName}
-			</Button>
-		</>
-	);
+  return (
+    <>
+      <div className="filter-btn">
+        <Button className="text-secondary rounded-5 border-1 border border-secondary" onClick={() => props.func()}>
+          {props.btnName}
+        </Button>
+      </div>
+    </>
+  );
 };
 
 export default RecipesFiltersBtn;
