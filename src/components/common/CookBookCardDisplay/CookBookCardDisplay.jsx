bd66a40b8782216dba5b6e40abaae5dc7188f589
Pierre-Marie <pierremariemarchio@gmail.com>
2024-05-29 15:06:53 +0200

    [chores] working

---
+++ src/components/common/CookBookCardDisplay/CookBookCardDisplay.jsx
@@ -0,0 +1,15 @@
+import './cookBookCardDisplay.scss'
+
+const CookBookCardDisplay = () => {
+
+    return (
+        <>
+        <div className='bg-cookbook-display'>
+
+        </div>
+        
+        </>
+    )
+};
+
+export default CookBookCardDisplay
