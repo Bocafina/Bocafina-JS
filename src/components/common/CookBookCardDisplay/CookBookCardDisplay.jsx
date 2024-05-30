e5b4695f99ddab54c8dbebb82d84e0f00f9b22ba
Pierre-Marie <pierremariemarchio@gmail.com>
2024-05-30 09:34:49 +0200

    chores

--- src/components/common/CookBookCardDisplay/CookBookCardDisplay.jsx
+++ src/components/common/CookBookCardDisplay/CookBookCardDisplay.jsx
@@ -18,7 +18,12 @@ const CookBookCardDisplay = () => {
 
   return (
     <>
-      <div className="bg-cookbook-display py-1 my-4 row mx-1 rounded-2">{cookBookRecipies}</div>
+      <div className="bg-cookbook-display py-1 my-4 row mx-1 rounded-2">
+        <div className="d-flex text-secondary mx-2 mt-2">
+          <h5>Mes recettes</h5>
+
+        </div>
+        {cookBookRecipies}</div>
     </>
   );
 };
