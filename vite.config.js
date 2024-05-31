1c8e47f10c8ed1c252e389feac3a31d648b94351
Pierre-Marie <pierremariemarchio@gmail.com>
2024-05-31 16:05:58 +0200

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/bocafina-react-js/",
   sourcemap: true,
   plugins: [react()],
 })
