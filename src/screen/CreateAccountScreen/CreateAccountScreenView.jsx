8d5684cc9e221400e06eda0d7e1a4b3b70a52894
Pierre-Marie <pierremariemarchio@gmail.com>
2024-05-31 15:14:43 +0200

    [screen] createAccoutScreen

--- src/screen/CreateAccountScreen/CreateAccountScreenView.jsx
+++ src/screen/CreateAccountScreen/CreateAccountScreenView.jsx
@@ -1,56 +1,71 @@
 import ProfilePic from "../../components/common/ProfilePic/ProfilePic";
-import { Button } from "react-bootstrap";
 
 import "./CreateAccountScreenView.scss";
 import BtnBlackGreen from "../../components/common/BtnBlackGreen/BtnBlackGreen";
+import BackButtonMoblie from "../../components/mobile/BackButton/BackButtonMobilel";
 
 const CreateAccountScreenView = (props) => {
   return (
     <>
       <header>
+        <BackButtonMoblie to="/profiles/login" btnText="Créer un profil" />
         <div className="d-flex justify-content-center m-4">
           <ProfilePic profilePicSrc="profilepiccat.png" />
         </div>
       </header>
 
-      <form className="input-group text-secondary">
-        <label className="mb-1 ms-3" htmlFor="userName">
-          nom utilisateur
-        </label>
-        <br />
-        <input
-          className="w-100 mb-3 mx-3 p-1 rounded-3 bg-transparent"
-          name="userName"
-          id="userName"
-          placeholder={props.userUUID}
-        />
-        <br />
+      <form className="input-group text-secondary" noValidate>
+        <div className="input-group position-relative d-flex mb-2">
+          <label className="mb-1 ms-3" htmlFor="userName">
+            nom utilisateur *
+          </label>
 
-        <label className="mb-1 ms-3" htmlFor="email-CA">
-          email
-        </label>
-        <br />
-        <input className="w-100 mb-3 mx-3 p-1 rounded-3 bg-transparent" name="email-CA" id="email-CA" />
-        <br />
+          <input
+            className="w-100  mx-3 p-1 rounded-3 bg-transparent inputStyle form-control"
+            name="userName"
+            id="userName"
+            placeholder={props.userUUID}
+          />
+          <span className="ms-3"> <small>Il sera affiché sur votre profil et vos recettes.</small></span>
 
-        <label className="mb-1 ms-3" htmlFor="password-CA">
-          mot de passe
-        </label>
-        <br />
-        <input className="w-100 mb-3 mx-3 p-1 rounded-3 bg-transparent" name="password-CA" id="password-CA" />
-        <br />
+          <div></div>
+        </div>
 
-        <label className="mb-1 ms-3" htmlFor="password-verification-CA">
-          vérification mot de passe
-        </label>
-        <br />
-        <input
-          className="w-100 mb-3 mx-3 p-1 rounded-3 bg-transparent"
-          name="password-verification-CA"
-          id="password-verification-CA"
-        />
-        <br />
-        <div className="d-flex justify-content-center mt-2 w-100">
+        <div className="input-group position-relative d-flex">
+          <label className="mb-1 ms-3" htmlFor="email-CA">
+            email *
+          </label>
+          <input
+            className="w-100 mb-4 mx-3 p-1 rounded-3 bg-transparent inputStyle form-control"
+            name="email-CA"
+            id="email-CA"
+          />
+        </div>
+
+        <div className="input-group position-relative d-flex">
+          <label className="mb-1 ms-3" htmlFor="password-CA">
+            mot de passe *
+          </label>
+          <input
+            className="w-100 mb-1 mx-3 p-1 rounded-3 bg-transparent inputStyle form-control"
+            name="password-CA"
+            id="password-CA"
+          />
+        </div>
+
+        <div className="input-group position-relative d-flex ">
+          <label className="mb-1 ms-3" htmlFor="password-verification-CA">
+            vérification mot de passe *
+          </label>
+          <input
+            className="w-100  mx-3 p-1 rounded-3 bg-transparent inputStyle form-control"
+            name="password-verification-CA"
+            id="password-verification-CA"
+          />
+          <span className="ms-3"> <small>champs obligatoires *</small></span>
+        </div>
+
+        <div className="d-flex justify-content-center w-100">
           <div className="btn-createAccount ">
             <BtnBlackGreen btnName="enregistrer" />
           </div>
--- src/screen/LoginScreen/LoginScreenView.jsx
+++ src/screen/LoginScreen/LoginScreenView.jsx
@@ -5,72 +5,69 @@ import ProfilePic from "../../components/common/ProfilePic/ProfilePic";
 
 import "./loginScreenView.scss";
 import BtnBlackGreen from "../../components/common/BtnBlackGreen/BtnBlackGreen";
+import BackButtonMoblie from "../../components/mobile/BackButton/BackButtonMobilel";
 
 const LoginScreenView = () => {
   return (
     <>
+      <BackButtonMoblie to="/" btnText="Login" />
       {/* header page */}
       <div className="flex-between mb-4">
         <div className="flex-between ms-2 mt-3">
           <div>
-            <Link to="">
-              <ProfilePic profilePicSrc="profilepiccat.png" />
-            </Link>
+            <ProfilePic profilePicSrc="profilepiccat.png" />
           </div>
           <div>
             <h4 className="text-secondary">CatUserUUID</h4>
           </div>
         </div>
         <div className="posSettings">
+          <Link to="/settings">
           <Image className="" src="../../src/assets/svg/settings.svg" alt="Icone des paramètres" />
+          </Link>
+          
         </div>
       </div>
 
       {/* ligne de séparation */}
       <hr className="text-secondary opacity-line full-width-line" />
 
-			{/* formulaire de connexion */}
-			<form className="input-group text-secondary" novalidate>
-				<label className="mb-1 ms-3" htmlFor="email">
-					email
-				</label>
-				<br />
-				<input
-					className="w-100 mb-3 mx-3 p-2 rounded-3 bg-transparent inputStyle form-control"
-					name="email"
-					id="email"
-					placeholder="(exemple: votrenom@domaine.com)"
-					required
-				/>
-				<br />
-				<div className="input-group position-relative d-flex justify-content-between w-100">
-					{/* image du chat qui grimpe */}
+      {/* formulaire de connexion */}
+      <form className="input-group text-secondary" noValidate>
+        <label className="mb-1 ms-3" htmlFor="email">
+          email
+        </label>
+        <br />
+        <input
+          className="w-100 mb-3 mx-3 p-2 rounded-3 bg-transparent inputStyle form-control"
+          name="email"
+          id="email"
+          placeholder="(exemple: votrenom@domaine.com)"
+          required
+        />
+        <br />
+        <div className="input-group position-relative d-flex justify-content-between w-100">
+          {/* image du chat qui grimpe */}
 
-					<label className="mb-1 ms-3" htmlFor="password">
-						mot de passe
-					</label>
-					<div className="posCatClaws">
-						<Image
-							className=""
-							src="../../src/assets/img/scratch-gray.png"
-							alt="Image de chat qui grimpe"
-						/>
-					</div>
-					<Link to="">
-						<span className="me-3 sizeForgottenPassword text-secondary">
-							mot de passe oublié ?
-						</span>
-					</Link>
-				</div>
-				<input
-					className="w-100 mb-3 mx-3 p-2 rounded-3 bg-transparent inputStyle form-control"
-					type="password"
-					name="password"
-					id="password"
-					placeholder="Votre mot de passe"
-					required
-				/>
-			</form>
+          <label className="mb-1 ms-3" htmlFor="password">
+            mot de passe
+          </label>
+          <div className="posCatClaws">
+            <Image className="" src="../../src/assets/img/scratch-gray.png" alt="Image de chat qui grimpe" />
+          </div>
+          <Link to="">
+            <span className="me-3 sizeForgottenPassword text-secondary">mot de passe oublié ?</span>
+          </Link>
+        </div>
+        <input
+          className="w-100 mb-3 mx-3 p-2 rounded-3 bg-transparent inputStyle form-control"
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Votre mot de passe"
+          required
+        />
+      </form>
 
       {/* bouton de connection + rester ou non connecté */}
       <div className="btn-connexion mt-5">
