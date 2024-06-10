8b2da87893f7f5ebb8ea3145d5a0cb52a441419f
Sam Sabat <samnsabat@gmail.com>
2024-06-10 20:33:38 +0200

    navbar fixed ok - header next

--- src/components/desktop/NavbarDesktop/NavbarDesktop.jsx
+++ src/components/desktop/NavbarDesktop/NavbarDesktop.jsx
@@ -13,35 +13,42 @@ import SocialMedias from "./SocialMedias/SocialMedias";
 const NavbarDesktop = () => {
 	return (
 		<>
-			<aside className="bg-primary me-2 p-1 rounded-3 d-flex flex-column justify-content-between d-none d-md-block">
-				<div className="">
-					<Logo />
-					<ProfilePic profilePicSrc="profilepiccat.png" />
-					<BtnBlackGreen btnName="modifier le profil" />
-					<hr className="text-secondary mx-3 border border-1" />
+			<aside className="bg-primary me-2 p-1 rounded-3 d-none d-md-block">
+				<div className="d-flex flex-column justify-content-between flex-grow-1 h-100">
+					<div className="d-flex flex-column">
+						<Logo />
+						<ProfilePic profilePicSrc="profilepiccat.png" />
+						<BtnBlackGreen btnName="modifier le profil" />
+						<hr className="text-secondary mx-3 border border-1" />
 
-					<Nav className="gap-3">
-						<NavbarIcon icon="home" name="Accueil" as={Link} to="/" />
-						<NavbarIcon icon="search" name="Recherche" as={Link} to="/search" />
-						<NavbarIcon
-							icon="cookbook"
-							name="Cookbook"
-							as={Link}
-							to="/cookbook"
-						/>
-						<NavbarIcon icon="menus" name="Planner" as={Link} to="/planner" />
-						<NavbarIcon
-							icon="groceries"
-							name="Courses"
-							as={Link}
-							to="/groceries"
-						/>
-					</Nav>
-					<hr className="text-secondary mx-3 border border-1" />
-				</div>
-				<div className="d-flex flex-column align-items-center justify-content-center">
-					<SocialMedias />
-					<Contact />
+						<Nav className="gap-4 my-5">
+							<NavbarIcon icon="home" name="Accueil" as={Link} to="/" />
+							<NavbarIcon
+								icon="search"
+								name="Recherche"
+								as={Link}
+								to="/search"
+							/>
+							<NavbarIcon
+								icon="cookbook"
+								name="Cookbook"
+								as={Link}
+								to="/cookbook"
+							/>
+							<NavbarIcon icon="menus" name="Planner" as={Link} to="/planner" />
+							<NavbarIcon
+								icon="groceries"
+								name="Courses"
+								as={Link}
+								to="/groceries"
+							/>
+						</Nav>
+						<hr className="text-secondary mx-3 border border-1" />
+					</div>
+					<div className="d-flex flex-column align-items-center">
+						<SocialMedias />
+						<Contact />
+					</div>
 				</div>
 			</aside>
 		</>
@@ -50,8 +57,7 @@ const NavbarDesktop = () => {
 
 export default NavbarDesktop;
 
-
-// as={Link} si NavbarIcon est conçu pour être utilisé avec React Router, 
+// as={Link} si NavbarIcon est conçu pour être utilisé avec React Router,
 // alors oui, vous devez inclure as={Link} dans votre code.
-// Cela garantira que les liens sont correctement gérés par React Router 
-// et qu'ils utilisent la navigation de l'application au lieu du comportement par défaut des liens HTML. 
+// Cela garantira que les liens sont correctement gérés par React Router
+// et qu'ils utilisent la navigation de l'application au lieu du comportement par défaut des liens HTML.
