924be2880ada484a15d2e510a4caa8e04b076cfc
Sam Sabat <samnsabat@gmail.com>
2024-06-19 17:54:46 +0200

    hot fix desktop navbar fixed and header/main global sizes

--- src/app/core/App.jsx
+++ src/app/core/App.jsx
@@ -7,19 +7,23 @@ import Routing from "./Routes";
 
 function App() {
   return (
-    <>
-      <Container fluid className="app-container d-flex g-0">
-        <NavbarDesktop />
-        <div className="d-flex flex-column flex-grow-1">
-          <Header />
-          <main>
-            <Routing />
-          </main>
-        </div>
-      </Container>
-      <NavbarMobile />
-    </>
-  );
+		<>
+			<Container fluid className="app-container d-flex g-0">
+				<NavbarDesktop />
+				<div className="d-flex flex-column flex-grow-1">
+					<div className="app-header">
+						<Header />
+					</div>
+					<div className="app-content flex-grow-1">
+						<main>
+							<Routing />
+						</main>
+					</div>
+				</div>
+			</Container>
+			<NavbarMobile />
+		</>
+	);
 }
 
 export default App;
--- src/app/core/layout/desktop/NavbarDesktop/NavbarDesktop.jsx
+++ src/app/core/layout/desktop/NavbarDesktop/NavbarDesktop.jsx
@@ -12,32 +12,49 @@ import SocialMedias from "../SocialMedias/SocialMedias";
 
 const NavbarDesktop = () => {
   return (
-    <>
-      <aside className="bg-primary me-2 p-1 rounded-3 d-none d-md-block">
-        <div className="d-flex flex-column justify-content-between h-100">
-          <div className="d-flex flex-column">
-            <Logo />
-            <ProfilePic profilePicSrc="profilepiccat.png" />
-            <BtnBlackGreen btnName="se connecter" />
-            <hr className="text-secondary mx-3 border border-1" />
+		<>
+			<aside className="navbar-desktop bg-primary me-2 p-1 rounded-3 position-fixed d-none d-md-block">
+				<div className="d-flex flex-column justify-content-between h-100">
+					<div className="d-flex flex-column ">
+						<Logo />
+						<ProfilePic profilePicSrc="profilepiccat.png" />
+						<Link to="/profiles/login">
+							<BtnBlackGreen btnName="se connecter" />
+						</Link>
+						<hr className="text-secondary mx-3 border border-1" />
 
-            <Nav className="gap-4 my-5">
-              <NavbarIcon icon="home" name="Accueil" as={Link} to="/" />
-              <NavbarIcon icon="search" name="Recherche" as={Link} to="/search" />
-              <NavbarIcon icon="cookbook" name="Cookbook" as={Link} to="/cookbook" />
-              <NavbarIcon icon="menus" name="Planner" as={Link} to="/planner" />
-              <NavbarIcon icon="groceries" name="Courses" as={Link} to="/groceries" />
-            </Nav>
-            <hr className="text-secondary mx-3 border border-1" />
-          </div>
-          <div className="d-flex flex-column align-items-center">
-            <SocialMedias />
-            <Contact />
-          </div>
-        </div>
-      </aside>
-    </>
-  );
+						<Nav className="gap-4 my-5">
+							<NavbarIcon icon="home" name="Accueil" as={Link} to="/" />
+							<NavbarIcon
+								icon="search"
+								name="Recherche"
+								as={Link}
+								to="/search"
+							/>
+							<NavbarIcon
+								icon="cookbook"
+								name="Cookbook"
+								as={Link}
+								to="/cookbook"
+							/>
+							<NavbarIcon icon="menus" name="Planner" as={Link} to="/planner" />
+							<NavbarIcon
+								icon="groceries"
+								name="Courses"
+								as={Link}
+								to="/groceries"
+							/>
+						</Nav>
+						<hr className="text-secondary mx-3 border border-1" />
+					</div>
+					<div className="d-flex flex-column align-items-center">
+						<SocialMedias />
+						<Contact />
+					</div>
+				</div>
+			</aside>
+		</>
+	);
 };
 
 export default NavbarDesktop;
