b27acc39e7bffce0270cf086655d147e6853dbbb
Sam Sabat <samnsabat@gmail.com>
2024-06-23 20:50:29 +0200

    accordéon planner

--- src/app/modules/cookBook/components/CookBookCardDisplay/CookBookCardDisplay.jsx
+++ src/app/modules/cookBook/components/CookBookCardDisplay/CookBookCardDisplay.jsx
@@ -5,28 +5,28 @@ import "./cookbookCardDisplay.scss";
 import RecipesCardDisplay from "../../../../shared/components/RecipeCard/RecipeCard";
 
 const CookbookCardDisplay = (props) => {
-  const cookbookRecipes = props.recipes.map((recipes) => (
-    <Col xs={6} sm={6} md={6} lg={6} xl={4} xxl={3} key={recipes.id}>
-    <RecipesCardDisplay
-      shadow="shadow-card"
-      hasHeader={false}
-      imageSrc={recipes.imageSrc}
-      profilName={recipes.profilName}
-      recipeTitle={recipes.recipeTitle}
-    />
-    </Col>
-  ));
+	const cookbookRecipes = props.recipes.map((recipes) => (
+		<Col xs={6} sm={6} md={6} lg={6} xl={4} xxl={3} key={recipes.id}>
+			<RecipesCardDisplay
+				shadow="shadow-card"
+				hasHeader={false}
+				imageSrc={recipes.imageSrc}
+				profilName={recipes.profilName}
+				recipeTitle={recipes.recipeTitle}
+			/>
+		</Col>
+	));
 
-  return (
-    <>
-      <Row className="bg-cookbook-display py-1 my-4 mx-1 rounded-3">
-        <div className="d-flex text-secondary mx-2 mt-2">
-          <h5>Mes recettes</h5>
-        </div>
-        {cookbookRecipes}
-      </Row>
-    </>
-  );
+	return (
+		<>
+			<Row className="bg-cookbook-display py-1 my-4 mx-1 rounded-3">
+				<div className="d-flex text-secondary mx-2 mt-2">
+					<h5>Mes recettes</h5>
+				</div>
+				{cookbookRecipes}
+			</Row>
+		</>
+	);
 };
 
 export default CookbookCardDisplay;
